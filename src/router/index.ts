926997f9f450c6610fc491341fcdc079b4380e29
ekunemmanuel <emmanuelapabiekun@gmail.com>
2026-07-29 18:35:07 +0100

    first commit

---
+++ src/router/index.ts
@@ -0,0 +1,11 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import index from '../pages/index.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [ 
+      { path: '/', name: 'index', component: index },
+  ],
+})
+
+export default router
